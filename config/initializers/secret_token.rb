369fee9bdf63d3334fd84932e588f8be8e70de2d
Baptiste Coulange <bc@cornis.fr>
2013-04-26 19:06:46 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CornisReports::Application.config.secret_token = 'a77e69ea81bbc30d719d7ca4845ab29b784e9de2768ffa781676f532a11900eb4e6d24b2f1782b41adcab34acad3be989a4be8e2268119690243d76b4fd0b67c'
